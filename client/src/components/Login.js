3af2e7007346e832cf76d4ad66b3415c1f8c0427
Drew26 <superdrewf26@hotmail.com>
2019-08-15 20:58:54 -0700

    export default

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -3,3 +3,10 @@ import React, { Component } from 'react';
 import { Link, Redirect } from 'react-router-dom';
 import axios from 'axios';
 import Nav from './children/Nav'
+
+
+require('./login.css');
+
+export default class Login extends Component {
+    
+}
