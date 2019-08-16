35910aa5310a1d2d5e865266c79bb68679f58c25
Drew26 <superdrewf26@hotmail.com>
2019-08-15 21:00:17 -0700

    login constructor

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -8,5 +8,17 @@ import Nav from './children/Nav'
 require('./login.css');
 
 export default class Login extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          username: '',
+          password: '',
+          redirectToReferrer: false
+        };
     
+        this.handleUsernameChange = this.handleUsernameChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.loginUser = this.loginUser.bind(this);
+      }
 }
