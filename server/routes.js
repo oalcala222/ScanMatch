724204f20455a6524271ba0a47cd392aeda5b0ef
Drew26 <superdrewf26@hotmail.com>
2019-08-20 20:36:33 -0700

    login test

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
+const users = require('./routes/users');
 
 
-
+app.use('/apis/users', users);
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -2,7 +2,25 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
+const passport = require("./config/passport");
+const session  = require('express-session'); 
+const config	= require("./config/extra-config");
 const PORT = process.env.PORT || 3001;
+
+// instantiate our app
+const app            = express();
+
+// Enable CORS from client-side
+app.use(function(req, res, next) {  
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+const authCheck = require('./config/middleware/attachAuthenticationStatus'); 
 
 // Configure body parsing for AJAX requests
 app.use(express.urlencoded({ extended: true }));
@@ -11,6 +29,11 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+app.use(session({ secret: config.sessionKey, resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(authCheck);
 
 // Add routes, both API and view
 app.use(routes);
