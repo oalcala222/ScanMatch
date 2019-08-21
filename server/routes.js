00c6d6809906f85ad196de783af950d827b11058
taco time <oalcala222@gmail.com>
2019-08-20 20:01:34 -0700

    some simple routes

--- server/routes.js
+++ server/routes.js
@@ -1,29 +1,10 @@
-
-const path = require("path");
-const router = require("express").Router();
-const apiRoutes = require("./routes");
-
-// API Routes
-router.use("/routes", apiRoutes);
-
-// If no API routes are hit, send the React app
-router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
-});
-
-module.exports = router;
-
 module.exports = function(app){
     // Our model controllers (rather than routes)
-    const users = require('./routes/users');
-    const trips = require('./routes/trips');
+    const xlupload = require('./routes/xldata');
+    const scanupload = require('./routes/scana');
 
-    const authCheckMiddleware = require('./config/middleware/authCheck');
-    app.use('/apis/trips', authCheckMiddleware);
-    app.use('/apis/pricing', authCheckMiddleware);
-
-    app.use('/apis/users', users);
-    app.use('/apis/trips', trips);
+    app.use('/uploadxl', xlupload);
+    app.use('/scanneddata', scanupload);
     //other routes..
 }
 
