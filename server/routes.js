b1182df2b0d26e755cc3a07544dc53b34145d258
taco time <oalcala222@gmail.com>
2019-08-20 17:57:41 -0700

    changes to server and some upload component files

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,13 @@
-const router = require("express").Router();
+module.exports = function(app){
+    // Our model controllers (rather than routes)
+    const users = require('./routes/users');
+    const trips = require('./routes/trips');
 
+    const authCheckMiddleware = require('./config/middleware/authCheck');
+    app.use('/apis/trips', authCheckMiddleware);
+    app.use('/apis/pricing', authCheckMiddleware);
 
-
-
-module.exports = router;
+    app.use('/apis/users', users);
+    app.use('/apis/trips', trips);
+    //other routes..
+}
