aaf884e8c6a56f47c8687aecbda803459dfc4d3d
RyFogle <13fogle@gmail.com>
2019-08-23 16:44:06 -0700

    adding component

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,11 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./components/Pages/Home";
 import Match from "./components/Pages/Match";
 import Scan from "./components/Pages/Scan";
-import Upload from "./components/Pages/UploadXL";
+import ExcelPage from "./components/Pages/ExcelPage";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
 import './App.css';
+import "antd/dist/antd.css";
 
 class App extends Component {
   render() {
@@ -17,7 +18,7 @@ class App extends Component {
             <Route exact path="/" component={Home} />
             <Route exact path="/match" component={Match} />
             <Route exact path="/scanning" component={Scan} />
-            <Route exact path="/upload" component={Upload} />
+            <Route exact path="/upload" component={ExcelPage} />
             <Route exact path="/signin" component={Login} />
             <Route exact path="/signup" component={Signup} />
           </Switch>
--- client/src/components/upload.js
+++ client/src/components/upload.js
@@ -1,3 +1,4 @@
 //we need a component that will handle the file input
 //we need a component to convert or save the file temporarily
 //small change
+
